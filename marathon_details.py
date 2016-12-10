49ca48c7d069e015efbc0a4dde187c5686b3bb85
Brad Montgomery <brad@bradmontgomery.net>
2016-12-10 15:22:15 -0600

    added an export function to generate a csv for all our data

--- marathon_details.py
+++ marathon_details.py
@@ -11,6 +11,7 @@ import sys
 from statistics import mean, median, mode, StatisticsError
 from collections import Counter
 
+import tablib
 import parsers
 
 
@@ -24,6 +25,10 @@ def is_numeric(value):
 
 
 def print_data(year):
+    """
+    Print some fun facts & stats for the given year's marathon.
+
+    """
     locale.setlocale(locale.LC_ALL, '')  # Use locale to pretty-print the combined distance run.
 
     # parse our data...
@@ -106,8 +111,82 @@ def print_data(year):
     else:
         print("Sorry, either no data or parser for {}.".format(year))
 
+
+def export_data():
+    """
+    Generate a CSV for all the years in which we have a parser.
+
+    """
+    # What we'll keep:
+    # - total_runners
+    # - num males
+    # - num females
+    # - num states
+    # - combined distance
+    # - average age (mean)
+    # - average pace (mean)
+    # - average finish time (mean)
+    headers = [
+        'Total Runners', 'Males', 'Females', 'States', 'Combined Distance',
+        'Mean Age', 'Mean Pace', 'Mean Finish Time',
+    ]
+    data = tablib.Dataset(headers=headers)
+
+    years = sorted([
+        year for year, parser in parsers.PARSERS.items()
+        if parser is not None
+    ])
+    for year in years:
+        # parse the the data fot he year
+        runners = parsers.parse(year)
+        row = []
+        if runners:
+
+            row.append(len(runners))  # Total Runners
+            row.append(len([runner.sex for runner in runners if runner.sex == "F"]))
+            row.append(len([runner.sex for runner in runners if runner.sex == "M"]))
+            row.append(len(set(runner.state for runner in runners)))  # Sates
+            row.append(int(len(runners) * 26.2))  # Combined Distance
+
+            # Average age.
+            ages = [int(runner.age) for runner in runners if is_numeric(runner.age)]
+            row.append(int(mean(ages)))
+
+            # Average pace.
+            paces = []
+            for runner in runners:
+                minutes, seconds = runner.pace.split(":")
+                paces.append(int(seconds) + (int(minutes) * 60))
+            mean_pace = int(mean(paces))
+            mean_pace_minutes, mean_pace_seconds = divmod(mean_pace, 60)
+            mean_pace = "{}:{}".format(mean_pace_minutes, mean_pace_seconds)
+            row.append(mean_pace)
+
+            # Average finish times.
+            times = []
+            for runner in runners:
+                hours, minutes, seconds = runner.time.split(":")
+                times.append(int(seconds) + (int(minutes) * 60) + (int(hours) * 3600))
+            mean_time = int(mean(times))
+            minutes, seconds = divmod(mean_time, 60)
+            hours, minutes = divmod(minutes, 60)
+            mean_time = "{}:{}:{}".format(hours, minutes, seconds)
+            row.append(mean_time)
+
+            # Append to our dataset
+            data.append(row)
+            print('Calculated data for {}...'.format(year))
+
+    with open("output.csv", "w") as csvfile:
+        csvfile.write(data.csv)
+        print("Output written to 'output.csv'")
+
+
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 2 and sys.argv[1].lower() == 'export':
+        export_data()
+    elif len(sys.argv) == 2:
         print_data(year=int(sys.argv[1]))
     else:
-        print("Usage: python marathon_details.py <year>")
+        print("USAGE:\n\tpython marathon_details.py <year>")
+        print("OR:\n\tpyton marathon_details.py export")
